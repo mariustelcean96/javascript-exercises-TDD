3d72a1a7aacacc6388ab10d348a6b44f768bf399
Telcean Marius-Andrei <marius.telcean96@gmail.com>
2022-03-19 14:41:32 +0200

    Upload to test with Jest on a Linux device.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(...arguments) {
+	return arguments.reduce((previous, current) => {
+    return previous + current;
+  })
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args.reduce((previous, current) =>
+  previous - current);
+};
+//Is there any difference between
+//add and sum functions?
+const sum = function(...args) { 
+	let sum = 0;
+  for(let arg of args) {
+    sum += arg;
+  }
+  return sum;
 };
 
-const sum = function() {
-	
+const multiply = function(...args) {
+ return args.reduce((previous, current) => previous * current);
 };
 
-const multiply = function() {
+const power = (...args) => args.reduce((current, present) => current ** present);
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(nr) {
+  if (nr === 0) return 1;
+  else {
+    let result = 1;
+	  for(let i = 1; i <= nr; i++) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
