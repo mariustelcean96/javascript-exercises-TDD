d1e717627c7bc478c627f8b39754a9ecd7ea434f
Marius-Andrei Telcean <marius.telcean96@gmail.com>
2022-03-23 15:21:31 +0200

    Give a possible solution for the task.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+let outputString = '';
+for (let i = inputString.length - 1; i >= 0 ; i--) {
+  outputString += inputString[i];
+}
+if (outputString === inputString) return true;
 };
 
 // Do not edit below this line
