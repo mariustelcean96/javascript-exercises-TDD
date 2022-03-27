73c140d101b2f21c6bf280479ff192e61367b33b
Marius-Andrei Telcean <marius.telcean96@gmail.com>
2022-03-27 21:48:31 +0300

    Upload a working protoype. Amazing.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
 const fibonacci = function() {
-
-};
+    let result = [];
+    for(let i = 0; i <= 10; i++) {
+    if (i === 0) {
+    result.push (0);
+    continue;
+    }
+    
+    if (i === 1) {
+    result.push(1);
+    continue;
+    
+    } else {
+        result.push(result[i - 1] + result[i - 2]); 
+        }
+    }
+    console.log(result);
 
 // Do not edit below this line
 module.exports = fibonacci;
