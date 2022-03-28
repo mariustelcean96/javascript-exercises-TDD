98b34d89072fcb904545d6c2222c4b252fe12d9d
Marius-Andrei Telcean <marius.telcean96@gmail.com>
2022-03-28 21:46:03 +0300

    Upload the solution.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,20 +1,20 @@
-const fibonacci = function() {
-    let result = [];
-    for(let i = 0; i <= 10; i++) {
+const fibonacci = function(string) {
+  let number = parseInt(string);
+  if(number < 0) return 'OOPS';
+  let result = [];
+  for(let i = 0; i <= number; i++) {
     if (i === 0) {
-    result.push (0);
-    continue;
+      result.push(0);
+      continue;
     }
-    
     if (i === 1) {
-    result.push(1);
-    continue;
-    
+      result.push(1);
+      continue;
     } else {
         result.push(result[i - 1] + result[i - 2]); 
-        }
     }
-    console.log(result);
-
+  }
+    return result[number];
+};
 // Do not edit below this line
 module.exports = fibonacci;
