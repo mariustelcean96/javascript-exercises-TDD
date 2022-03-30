590aaa59e99a178f347829b8671264d3a70196a3
Marius-Andrei Telcean <marius.telcean96@gmail.com>
2022-03-30 23:13:17 +0300

    Hallelujah!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,8 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(obj) {
+    const sortedPeople = obj.sort((a,b) => 
+    a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? 1 : -1);
+    return sortedPeople[0];
+   };
 
 // Do not edit below this line
 module.exports = findTheOldest;
