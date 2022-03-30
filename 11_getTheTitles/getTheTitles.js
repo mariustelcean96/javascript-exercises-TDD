6d620cecb03fca876e24ec17817aaf0976160e68
Marius-Andrei Telcean <marius.telcean96@gmail.com>
2022-03-30 20:42:03 +0300

    Upload code current version.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -2,8 +2,12 @@ const getTheTitles = function(books) {
   let solution = [];
   const booksTitles = books.map(element =>
     solution.push(element.title));
-    return solution;
+  return solution;
 };
 
+/*const getTheTitles = function(array) {
+  return array.map(book => book.title);
+};
+*/
 // Do not edit below this line
 module.exports = getTheTitles;
