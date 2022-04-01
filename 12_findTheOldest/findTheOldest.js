85dc6a40c3d98a47f9c7346170534f2a751ec46d
Marius-Andrei Telcean <marius.telcean96@gmail.com>
2022-04-01 23:27:19 +0300

    Upload code current version.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function(obj) {
-    const sortedPeople = obj.sort((a,b) => 
-    a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? 1 : -1);
+const findTheOldest = function(array) {
+    const sortedPeople = array.sort(function (a,b) {
+        if (a.yearOfDeath === undefined || b.yearOfDeath === undefined) {
+            a.yearOfDeath = new Date().getFullYear();
+            b.yearOfDeath = new Date().getFullYear();
+        }
+      if(a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth) {
+      return 1;
+      } else {
+      return -1;
+      }
+    });
     return sortedPeople[0];
    };
 
