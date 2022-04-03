c0dd4a5d28be03d57ec9ca0c33f8e28732cb4dbd
Marius-Andrei Telcean <marius.telcean96@gmail.com>
2022-04-03 21:47:57 +0300

    Upload code current version.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,6 @@
 const findTheOldest = function(array) {
     const sortedPeople = array.sort(function (a,b) {
-        if (a.yearOfDeath === undefined || b.yearOfDeath === undefined) {
+        if (!a.yearOfDeath || !b.yearOfDeath) {
             a.yearOfDeath = new Date().getFullYear();
             b.yearOfDeath = new Date().getFullYear();
         }
@@ -13,5 +13,21 @@ const findTheOldest = function(array) {
     return sortedPeople[0];
    };
 
+/*
+const findTheOldest = function(array) {
+  return array.reduce((oldest, currentPerson) => {
+    const oldestAge = retrieveAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = retrieveAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    return oldestAge < currentAge ? currentPerson : oldest;
+  })
+};
+
+function retrieveAge(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
+*/
 // Do not edit below this line
 module.exports = findTheOldest;
